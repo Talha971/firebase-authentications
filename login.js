1201c2233030f7c6b439f7e7cc235427da93a24c
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-01 23:37:33 +0500

    Add files via upload

--- login.js
+++ login.js
@@ -1,24 +1,22 @@
-import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
-import { auth } from './config.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth } from "./config.js";
 
+// const form = document.querySelector("#form");
+// const email = document.querySelector("#email");
+// const password = document.querySelector("#password");
 
-const form = document.querySelector('#form')
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
-
-
-form.addEventListener('submit', (ahsanL) => {
-    ahsanL.preventDefault();
-    signInWithEmailAndPassword(auth, email.value, password.value)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log(user);
-            window.location = 'home.html'
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage);
-        });
-})
+form.addEventListener("submit", (ahsanL) => {
+  ahsanL.preventDefault();
+  signInWithEmailAndPassword(auth, "xyz@gmail.com", "123456")
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      window.location = "home.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
