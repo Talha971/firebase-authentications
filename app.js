5d999c41615101e84a600ecd361b6ae59849f2ab
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-11-29 18:39:54 +0500

    singin with google feature added

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
-import { auth } from './config.js';
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth } from "./config.js";
 
+const provider = new GoogleAuthProvider();
 
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
-const form = document.querySelector('#form')
-const email = document.querySelector('#email')
-const password = document.querySelector('#password')
-
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    console.log(email.value);
-    console.log(password.value);
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-        .then((res) => {
-            const user = res.user;
-            console.log(user);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage);
-        });
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(email.value);
+  console.log(password.value);
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((res) => {
+      const user = res.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
+let login_google = document.getElementById("login_google");
+login_google.addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+});
